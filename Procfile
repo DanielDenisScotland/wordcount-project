d8c96f755aee7d59a1af1e0c6663261b0c8c20e4
ddenisiuk <ddenisiuk.sc@gmail.com>
2020-09-16 17:22:02 +0100

    My second commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn wordcount.wsgi --log file -
--- wordcount/settings.py
+++ wordcount/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gg1jso4)mle&uv1p8ha^nj1oc=u+$l7h+r@%+cms_586*@2iu$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://bidziba.herokuapp.com/']
 
 
 # Application definition
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
